2831ee6f8fd06bc35611b06a78c9e579d737fa29
renuka28 <mr.renukaprasad@gmail.com>
2020-02-26 09:47:53 -0600

    Deep Learning - Course 5 - Week 1 - Building a recurrent neural network - step by step - Solution

--- Deep_Learning_Specialization_Coursera_Andrew_Ng/Course_1_Neural_Networks_and_Deep_Learning/Week_2/Logistic_Regression_with_a_Neural_Network_mindset/lr_utils.py
+++ Deep_Learning_Specialization_Coursera_Andrew_Ng/Course_1_Neural_Networks_and_Deep_Learning/Week_2/Logistic_Regression_with_a_Neural_Network_mindset/lr_utils.py
@@ -1,20 +1,33 @@
 import numpy as np
 import h5py
-    
-    
+import os
+
+
 def load_dataset():
-    train_dataset = h5py.File('datasets/train_catvnoncat.h5', "r")
-    train_set_x_orig = np.array(train_dataset["train_set_x"][:]) # your train set features
-    train_set_y_orig = np.array(train_dataset["train_set_y"][:]) # your train set labels
+    dir_name = os.path.dirname(os.path.abspath(__file__))
+    full_train_file_name = os.path.join(dir_name, "datasets")
+    full_train_file_name = os.path.join(
+        full_train_file_name, "train_catvnoncat.h5")
+    # print(full_train_file_name)
+    train_dataset = h5py.File(full_train_file_name, "r")
+    # your train set features
+    train_set_x_orig = np.array(train_dataset["train_set_x"][:])
+    train_set_y_orig = np.array(
+        train_dataset["train_set_y"][:])  # your train set labels
 
-    test_dataset = h5py.File('datasets/test_catvnoncat.h5', "r")
-    test_set_x_orig = np.array(test_dataset["test_set_x"][:]) # your test set features
-    test_set_y_orig = np.array(test_dataset["test_set_y"][:]) # your test set labels
+    full_test_file_name = os.path.join(dir_name, "datasets")
+    full_test_file_name = os.path.join(
+        full_test_file_name, "test_catvnoncat.h5")
+    # print(full_test_file_name)
+    test_dataset = h5py.File(full_test_file_name, "r")
+    # your test set features
+    test_set_x_orig = np.array(test_dataset["test_set_x"][:])
+    test_set_y_orig = np.array(
+        test_dataset["test_set_y"][:])  # your test set labels
 
-    classes = np.array(test_dataset["list_classes"][:]) # the list of classes
-    
+    classes = np.array(test_dataset["list_classes"][:])  # the list of classes
+
     train_set_y_orig = train_set_y_orig.reshape((1, train_set_y_orig.shape[0]))
     test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig.shape[0]))
-    
-    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
 
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig, classes
---
+++ Deep_Learning_Specialization_Coursera_Andrew_Ng/Course_1_Neural_Networks_and_Deep_Learning/Week_2/Logistic_Regression_with_a_Neural_Network_mindset/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    # m_train = train_set_x_orig.shape[0]
+    # m_test = test_set_x_orig.shape[0]
+    # num_px = train_set_x_orig.shape[1]
+    # print(train_set_x_orig.shape)
